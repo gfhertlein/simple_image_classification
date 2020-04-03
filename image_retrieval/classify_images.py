b5c61b3828deffe762e9fa5d8ddb9f5200155c05
Fred Hertlein <gfhertlein@users.noreply.github.com>
2020-04-03 09:01:51 -0700

    Update classify_images.py
    Changed tensorflow import to be v1 compatible

--- image_retrieval/classify_images.py
+++ image_retrieval/classify_images.py
@@ -57,7 +57,8 @@ import psutil
 from collections import defaultdict
 import numpy as np
 from six.moves import urllib
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 FLAGS = tf.app.flags.FLAGS
 
